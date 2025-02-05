e0dc0c65bcf971532e142c1471c2e8c97e0a2a1e
Danboi <168639806+Danboi01@users.noreply.github.com>
2025-02-05 16:53:04 +1300

    Updated fxmanifest.lua

--- atlasbansync/fxmanifest.lua
+++ atlasbansync/fxmanifest.lua
@@ -5,7 +5,8 @@ description 'Syncs Discord bans to your FiveM server upon user connect'
 version '1.0'
 lua54 'yes'
 
-server_scripts {
-    'server/server.lua',
-    'config.lua',
-}
+shared_script 'config.lua'
+
+server_script 'server/server.lua'
+
+
